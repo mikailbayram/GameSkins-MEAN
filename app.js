40a239f936f479b63ad8908f7f215ed6705a12a0
Mikail <mikailbosna98@gmail.com>
2018-10-20 17:24:44 +0200

    added dotenv

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
+require('dotenv').config();
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 app.listen(process.env.PORT, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port!'+process.env.PORT);
 });
