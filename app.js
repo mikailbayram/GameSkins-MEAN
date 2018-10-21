1fd0ecabfdebbd4229b5b96ad30a5727febc7617
Mikail <mikailbosna98@gmail.com>
2018-10-21 14:15:53 +0200

    add angular

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require("http");
 
 require('dotenv').config();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-app.listen(process.env.PORT, function () {
-    console.log('Example app listening on port!'+process.env.PORT);
-});
+app.use('/', express.static('app'));
+app.use(express.json());       // to support JSON-encoded bodies
+
+app.listen(process.env.PORT, () => console.log('Example app listening on port '+process.env.PORT));
