275e4955e0b27c3e3ac8016208a30d045245adc1
Mikail <mikailbosna98@gmail.com>
2019-01-01 12:08:02 +0100

    toast on invalid login

--- app/controllers/auth-controller.js
+++ app/controllers/auth-controller.js
@@ -1,3 +1,12 @@
-function AuthController($scope, $rootScope, $http) {
-    console.log("Hello from auth controller");
-}
+
+app.controller('authController', function ($scope, $http, $mdToast, $log) {
+
+    $scope.userLogin = function (user) {
+        $http.post('/user/login', user).then(function (response) {
+            localStorage.setItem('token', response.data.token)
+        }).catch(function (err) {
+            $mdToast.show($mdToast.simple().position('top right').textContent('Invalid Credentials!'));
+        })
+    }
+
+})
