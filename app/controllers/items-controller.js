195ba851d2e6e23c8d6df60c9ab6e1052e06af2f
Mikail <mikailbosna98@gmail.com>
2019-01-01 13:30:28 +0100

    add new item dialog

--- app/controllers/items-controller.js
+++ app/controllers/items-controller.js
@@ -1,3 +1,15 @@
-function ItemsController($scope, $rootScope, $http) {
-    
+function ItemsController($scope, $mdDialog) {
+    $scope.showDialog = function (evt) {
+        $mdDialog.show({
+            templateUrl: '../views/create-item-modal.html',
+            parent: angular.element(document.body),
+            targetEvent: evt,
+            clickOutsideToClose: true
+        })
+            .then(function (answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function () {
+                $scope.status = 'You cancelled the dialog.';
+            });
+    }
 }
