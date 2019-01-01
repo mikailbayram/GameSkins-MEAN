ff22a7ce6073445826372e10092b6bfd037b98c1
Mikail <mikailbosna98@gmail.com>
2019-01-01 10:49:21 +0100

    login api

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const getDb = require('../db').getDb;
+const jwt = require("jsonwebtoken");
 
-
+const jwt_secret = "secret_moj";
 
 router.post('/register', function (req, res) {
     const db = getDb();
@@ -19,6 +20,34 @@ router.post('/register', function (req, res) {
         }
     );
 });
+
+router.post('/login', function (req, res) {
+    const db = getDb();
+    const user = req.body;
+
+    db.collection("users").findOne(
+        { email: user.email, password: user.password },
+        function (error, user) {
+            if (error) {
+                throw error;
+            } else {
+                if (user) {
+                    var token = jwt.sign(user, jwt_secret, {
+                        expiresIn: 20000
+                    });
+
+                    res.send({
+                        success: true,
+                        message: "Authenticated",
+                        token: token
+                    });
+                } else {
+                    res.status(401).send("Credentials are wrong.");
+                }
+            }
+        }
+    );
+})
 
 router.get('/all', function (req, res) {
     const db = getDb();
